034ddb9be25ba4d41894f07239802d601a6a3ee7
Nick <rubert@rubert-Inspiron-N5110.(none)>
2012-02-16 15:59:53 -0600

    renamed file

--- rfdFilesToStrainImage.py
+++ rfdFilesToStrainImage.py
@@ -2,7 +2,7 @@
 '''Take 3 filenames as input:
     pre RFD file
     post RFD file
-    output strain file'''
+    output strain image'''
 
 from blockMatch import blockMatchClass
 from sys import argv
@@ -10,9 +10,14 @@ import os
 
 if len(argv) < 3:
     print "Error in usage.  Correct usage: "
-    print os.path.basename(argv[0]) + " rfdFile1 itkFile"
+    print os.path.basename(argv[0]) + " rfdFile1 rfdFile2 pngFile <maxStrain>"
+
+if len(argv) > 3:
+    vMax = float( argv[3] )
+
+else:
+    vMax = .02
 
 bl = blockMatchClass(argv[1],'rfd')
-
-for fNo in range(bl.nFrames - 1):
-    bl.CreateStrainImage(pngFileName=argv[2] + str(fNo).zfill(3) + '.png', preFrame = fNo , vMax = .02, skipFrame = 1)
+bl.SetRoiBoxSelect()
+bl.CreateStrainImage(pngFileName=argv[2] + str(fNo).zfill(3) + '.png', preFrame = fNo , vMax = .02, skipFrame = 1)
