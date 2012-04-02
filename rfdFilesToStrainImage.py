651a7d394e82894bc4591001c462f8037322bb2c
Nicholas Rubert <rubert@rubert-Inspiron-N5110.(none)>
2012-04-02 17:38:27 -0500

    Added ability to change scale on strain image

--- rfdFilesToStrainImage.py
+++ rfdFilesToStrainImage.py
@@ -2,22 +2,60 @@
 '''Take 3 filenames as input:
     pre RFD file
     post RFD file
-    output strain image'''
+    output strain image
+    It's a script for my attenuation estimation experiment to
+    spit out a strain image given 1-frame pre and post compression
+    RFD files.'''
 
 from blockMatch import blockMatchClass
 from sys import argv
 import os
+from matplotlib import pyplot as plt
 
 if len(argv) < 3:
     print "Error in usage.  Correct usage: "
     print os.path.basename(argv[0]) + " rfdFile1 rfdFile2 pngFile <maxStrain>"
 
-if len(argv) > 3:
-    vMax = float( argv[3] )
+if len(argv) > 4:
+    vMax = float( argv[4] )
 
 else:
     vMax = .02
 
-bl = blockMatchClass(argv[1],'rfd')
-bl.SetRoiBoxSelect()
-bl.CreateStrainImage(pngFileName=argv[2] + str(fNo).zfill(3) + '.png', preFrame = fNo , vMax = .02, skipFrame = 1)
+if '.png' not in argv[3]:
+    picFName = argv[3] + '.png'
+else:
+    picFName = argv[3]
+
+bl = blockMatchClass(argv[1],'rfd',postFile = argv[2], selectRoi = True)
+bl.CreateStrainImage(pngFileName=picFName, vMax = vMax)
+
+plt.imshow(bl.strainRGB, extent = [0, bl.fovX, bl.fovY, 0] , vmax = vMax, cmap = 'gray')
+plt.colorbar()
+plt.show()
+
+answer = None
+while answer is not 'Y' and answer is not 'N':
+    answer = raw_input('Is the plotting scale satisfactory? (Y or N)?\n')
+
+if answer is 'Y':
+    satisfiedWithScale = True
+else:
+    satisfiedWithScale = False
+
+while not satisfiedWithScale:
+    newMax = float( raw_input('Enter a new maximum strain value to display\n') )
+    print '\n The new maximum is: ' + str(newMax)
+    bl.RescaleRgbStrainImage(newMax)
+    plt.imshow(bl.strainRGB, extent = [0, bl.fovX, bl.fovY, 0] , vmax = newMax, cmap = 'gray')
+    plt.colorbar()
+    plt.show()
+   
+    answer = None
+    while answer is not 'Y' and answer is not 'N':
+        answer = raw_input('Is the plotting scale satisfactory? (Y or N)? \n')
+
+    if answer is 'Y':
+        satisfiedWithScale = True
+    else:
+        satisfiedWithScale = False
